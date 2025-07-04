20f9a6a48899daad8ac5045ae2b96ef287f2e516
Yasar Mahmood <yasarmahmood7x@gmail.com>
2025-07-04 10:55:32 +0530

    Blah Blah Added two lines

--- New1.java
+++ New1.java
@@ -1,5 +1,7 @@
 public class New1 {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("This is a new Java file.");
+        System.out.println("It contains a simple main method.");
     }
 }
